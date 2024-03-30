030f461f088887fe64ac8550e9188b385764724f
F1ZZ <TheKubonn@hotmail.com>
2024-03-30 11:04:33 +0100

    Added idle animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,32 +6,45 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-
+    // Setting up moveSpeed float
     [SerializeField] private float moveSpeed = 7f;
+
     private void Update()
     {
+        // Making a Vector2 which will handle input
         Vector2 inputVector = new Vector2(0, 0);
-        
+
+        // All keys pressed
         if (Input.GetKey(KeyCode.W))
         {
             inputVector.y = +1;
         }
+        
         if (Input.GetKey(KeyCode.S))
         {
             inputVector.y = -1;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             inputVector.x = -1;
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             inputVector.x = +1;
         }
 
+        // We are making inputVector normalized so that movement is smooth when pressing W & D for example
         inputVector = inputVector.normalized;
 
+        // Making a Vector3 which is direction of movement and moving only our player in x and z
+        // so we take it from our input vector
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        transform.position += moveDir * (Time.deltaTime * moveSpeed);
+
+        // Making character smoothly turn with function Slerp
+        const float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
